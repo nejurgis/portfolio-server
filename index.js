871e818867b26ef0e3b89430b000d7007b5c8f34
quietQuiet <j.lietunovas@gmail.com>
2019-05-21 13:50:13 +0200

    modified the server for some speeeed

--- index.js
+++ index.js
@@ -2,77 +2,35 @@ let app = require('express')()
 let http = require('http').Server(app)
 let io = require('socket.io')(http)
 
-const fs = require('fs')
+
+// const fs = require('fs')
 
 // app.use(cors())
 
+let coords = []
 
 io.on('connection', socket => {
   console.log('a user connected');
-  const cursorPositions = []
-  // socket.json.send
   socket.on('load history', ()=> {
-    // console.log('here you go')
-    fs.readFile('./hoverData.json', (err,body) => {
-      let history = JSON.parse(body)
-      console.log('history:', history)
-      io.emit('here you go', history)
-    })
-    
+      socket.emit('here you go', coords)
+      // console.log('on loading history coords.length is:', coords.length)
+
   })
   socket.on("hell", (msg)=> {
-    console.log('msg before stringify:', msg)
-    let history = JSON.stringify(msg)
-    console.log('msg after stringify:', history)
-    if (Array.isArray(msg)) {
-      // console.log('array is ok')
+    // console.log('when its hell coords.length is:', coords.length)
+
+    if (coords.length >= 700) {
+      
+      coords.shift()
+      // console.log('shifted the coords')
     } else {
-      console.log('ALARM')
+      
+      coords.push(msg)
+      // console.log('msg:', msg)
+      // console.log('coords', coords)
     }
-
-    fs.readFile('./hoverData.json',  (err,body) => {
-      let validateJson = (body) => {
-        try{
-          let data = JSON.parse(body)
-          return data
-        } catch(e) {
-          console.log('oops')
-          let data = new Array;
-          console.log('error:', e)
-          return data
-        }
-
-      } 
-      if (err) throw err
-      let data = validateJson(body)
-      if (data){
-        if (data.length >= 150) {
-          data.shift()
-        } else {
-          if (Array.isArray(msg)) {
-
-            data.push(msg)
-          } else {
-            // file.push([])
-            console.log('not an array!')
-          }
-        }
-        // need to stringify it back for json to get it
-        const json = JSON.stringify(data)
-        
-        io.emit('livestream', msg)
-        console.log('msg:', msg)
-        fs.writeFile('./hoverData.json', json,'utf8' , (err) => {
-          if (err) {
-            console.log('WRITIN AINT WORKIN')
-            throw err}
-            // console.log(`${msg} written to file`)
-          
-            
-          
-        })
-    }
-    })
+    io.emit('livestream', msg)
+    // console.log('when its livestream coords.length is:', coords.length)
   })
 
   socket.on('disconnect', function(){
